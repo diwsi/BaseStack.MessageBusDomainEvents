bd2343cb722c338a933b7ba9169fd03c19e94bfe
diwsi <xenamorphx@gmail.com>
2023-02-19 19:48:20 +0300

    initial

---
+++ DataIndexed.cs
@@ -0,0 +1,9 @@
+﻿namespace MessageBusDomainEvents
+{
+    public class DataIndexed
+    {
+        public Guid ID { get; set; }
+        public string? Name { get; set; }
+        public object? Value { get; set; }
+    }
+}
---
+++ IndexData.cs
@@ -0,0 +1,9 @@
+﻿namespace MessageBusDomainEvents
+{
+    public class IndexData
+    {
+        public Guid ID { get; set; }
+        public string? Name { get; set; }    
+        public object? Value { get; set; }
+    }
+}
